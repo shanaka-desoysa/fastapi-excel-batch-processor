ce8bae87276586e1e0b1edb75c2f9dbc27872252
Shanaka DeSoysa <desosha@chrobinson.com>
2023-01-05 13:35:12 -0600

    batch predict example

--- app/main.py
+++ app/main.py
@@ -1,47 +1,44 @@
+import io
+import tempfile
+
 from fastapi import FastAPI, File, HTTPException
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 import pandas as pd
-import geocoder
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Add a sample Excel file to the documentation
-sample_file_path = 'sample.xlsx'
-
-description = f"""
-Expects an excel file with columns: UN/LOCODE, City, State, ZIP Code <a href="/static/example_input.xlsx">Example Input</a>.<br>
-Returns a modified excel file with columns: UN/LOCODE, City, State, ZIP Code, Latitude, Longitude, Google Maps URL <a href="/static/example_output.xlsx">Example Output</a>
-"""
 
 
-@app.post("/batch/", description=description, response_description="Modified Excel file")
-async def batch_process(file: bytes = File(...)):
+def predict(feature_1, feature_2, feature_3):
+    # Simple model: predict 1 if feature_1 + feature_2 + feature_3 > 3, else predict 0
+    return 1 if feature_1 + feature_2 + feature_3 > 3 else 0
+
+
+@app.post("/predict_batch/")
+async def predict_batch(file: bytes = File(...)):
+    """
+    Expects an excel file with columns: feature_1, feature_2, feature_3.
+    <a href="/static/example_input.xlsx">Example Input</a>.<br>
+    Returns a modified excel file with columns: feature_1, feature_2, feature_3, prediction
+    <a href="/static/example_output.xlsx">Example Output</a>
+    """
     # Read the Excel file into a Pandas DataFrame
-    df = pd.read_excel(file)
-    
-    # Geocode the cities and add the latitude, longitude, and Google Maps URL columns to the DataFrame
-    latitudes = []
-    longitudes = []
-    google_maps_urls = []
-    for city, state, zip_code in df[['City', 'State', 'ZIP Code']].values:
-        address = f"{city}, {state} {zip_code}"
-        g = geocoder.arcgis(address)
-        latitudes.append(g.latlng[0])
-        longitudes.append(g.latlng[1])
-        google_maps_url = f"https://maps.google.com?q={g.latlng[0]},{g.latlng[1]}"
-        google_maps_urls.append(google_maps_url)
-    df['Latitude'] = latitudes
-    df['Longitude'] = longitudes
-    df['Google Maps URL'] = google_maps_urls
-    
-    # Create a temporary file to store the modified DataFrame
-    temp_file = '/tmp/modified.xlsx'
-    df.to_excel(temp_file, index=False)
-    
-    # Return the modified DataFrame as an Excel file response
-    headers = {
-        "Content-Disposition": "attachment; filename=modified.xlsx"
-    }
-    return FileResponse(temp_file, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers=headers)
+    df = pd.read_excel(io.BytesIO(file))
+
+    # Make predictions using the predict function and the apply method
+    df['prediction'] = df[['feature_1', 'feature_2', 'feature_3']].apply(
+        lambda row: predict(row['feature_1'], row['feature_2'], row['feature_3']), axis=1)
+
+    # Write the modified DataFrame to a temporary file and return it as an Excel file response
+    stream = io.BytesIO()
+    df.to_excel(stream, index=False)
+    stream.seek(0)
+    with tempfile.NamedTemporaryFile(mode="w+b", suffix=".xlsx", delete=False) as FOUT:
+        FOUT.write(stream.read())
+        return FileResponse(
+            FOUT.name,
+            media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            headers={
+                "Content-Disposition": "attachment; filename=predictions.xlsx",
+                "Access-Control-Expose-Headers": "Content-Disposition",
+            }
+        )
