382f326ca1d356ee0d9fe3096791e6ecc5381b61
Shanaka DeSoysa <desosha@chrobinson.com>
2023-01-05 11:44:12 -0600

    first working version

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# FastAPI Docker Template
-Template to quickly start a project with `FastAPI` and `docker-compose`.
+# Excel Batch Processor
+
+This is a simple batch processing service that accepts an Excel file, reads it into a Pandas DataFrame, adds a new column to the DataFrame, and returns the modified DataFrame as an Excel file.
+
+## Development
+
+To develop the app, clone the repository and create a virtual environment:
+
+```bash
+git clone https://github.com/myuser/excel-batch-processor.git
+cd excel-batch-processor
+python -m venv venv
+source venv/bin/activate
+```
+
+Install the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+Run the app using the ASGI server:
+
+```bash
+uvicorn app:app --reload
+```
+
+## Deployment
+
+To deploy the app using Docker, build the image and start the container:
+
+```bash
+docker-compose build
+docker-compose up
+```
--- app/main.py
+++ app/main.py
@@ -1,15 +1,47 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import pandas as pd
+import geocoder
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Add a sample Excel file to the documentation
+sample_file_path = 'sample.xlsx'
+
+description = f"""
+Expects an excel file with columns: UN/LOCODE, City, State, ZIP Code <a href="/static/example_input.xlsx">Example Input</a>.<br>
+Returns a modified excel file with columns: UN/LOCODE, City, State, ZIP Code, Latitude, Longitude, Google Maps URL <a href="/static/example_output.xlsx">Example Output</a>
+"""
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/batch/", description=description, response_description="Modified Excel file")
+async def batch_process(file: bytes = File(...)):
+    # Read the Excel file into a Pandas DataFrame
+    df = pd.read_excel(file)
+    
+    # Geocode the cities and add the latitude, longitude, and Google Maps URL columns to the DataFrame
+    latitudes = []
+    longitudes = []
+    google_maps_urls = []
+    for city, state, zip_code in df[['City', 'State', 'ZIP Code']].values:
+        address = f"{city}, {state} {zip_code}"
+        g = geocoder.arcgis(address)
+        latitudes.append(g.latlng[0])
+        longitudes.append(g.latlng[1])
+        google_maps_url = f"https://maps.google.com?q={g.latlng[0]},{g.latlng[1]}"
+        google_maps_urls.append(google_maps_url)
+    df['Latitude'] = latitudes
+    df['Longitude'] = longitudes
+    df['Google Maps URL'] = google_maps_urls
+    
+    # Create a temporary file to store the modified DataFrame
+    temp_file = '/tmp/modified.xlsx'
+    df.to_excel(temp_file, index=False)
+    
+    # Return the modified DataFrame as an Excel file response
+    headers = {
+        "Content-Disposition": "attachment; filename=modified.xlsx"
+    }
+    return FileResponse(temp_file, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers=headers)
